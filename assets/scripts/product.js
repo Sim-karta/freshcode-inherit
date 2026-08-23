57b0e767dc6b02564d17b996cae258570953492d
Sim-karta <simonenkonikita561@gmail.com>
2026-08-23 08:34:33 +0300

    Add method and getter

--- assets/scripts/product.js
+++ assets/scripts/product.js
@@ -1,11 +1,38 @@
 class FoodProduct {
-    constructor(title, category, brand) {
+    constructor(title, category, brand, manufactureDate) {
         this._title = title;
         this._category = category;
         this._brand = brand;
-        this._manufactureDate = new Date();
+        this._manufactureDate = manufactureDate;
+    }
+
+    get ageInDays() {
+        return Math.floor(
+            (new Date() - this._manufactureDate) / (1000 * 60 * 60 * 24),
+        );
+    }
+
+    getFullInfo() {
+        let days;
+        if (this.ageInDays < 1) {
+            days = "менше 1 дня";
+        } else if (this.ageInDays === 1) {
+            days = "1 день";
+        } else if (this.ageInDays > 1 && this.ageInDays < 5) {
+            days = this.ageInDays + " дні";
+        } else {
+            days = this.ageInDays + " днів";
+        }
+
+        return `Назва: ${this._title}, категорія: ${this._category}, бренд: ${this._brand}, вік товару: ${days}`;
     }
 }
 
-const product1 = new FoodProduct("Молоко 2.5%", "Молочка", "Яготинське");
+const product1 = new FoodProduct(
+    "Молоко 2.5%",
+    "Молочка",
+    "Яготинське",
+    new Date("2026-08-20"),
+);
 console.dir(product1);
+console.log(product1.getFullInfo());
